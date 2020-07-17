3de3a3161b3e5cfeab1a7739037a7055edf68653
Pablok-jpg <60201737+Pablok-jpg@users.noreply.github.com>
2020-07-17 08:30:06 -0500

    Add files via upload

---
+++ TarifaCajaCompensacion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CajaCompensacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su salario: ");
+            double salario = double.Parse(Console.ReadLine());
+
+            if (salario < (2 * 877803))
+            {
+                Console.WriteLine("Usted pertenece a la tarifa A.");
+            }
+            else if ((2 * 877803) <= salario && salario <= (351121))
+            {
+                Console.WriteLine("Usted pertenece a la tarifa B.");
+            }
+            else
+            {
+                Console.WriteLine("Usted pertenece a la tarifa C.");
+            }
+        }
+    }
+}
