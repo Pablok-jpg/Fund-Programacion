36adfb1238d5a5c89d429eb3c7cc9754d005def5
Pablok-jpg <60201737+Pablok-jpg@users.noreply.github.com>
2020-08-18 23:11:38 -0500

    Add files via upload

---
+++ Euleralax.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallemos el valor de euler ^x, ingrese x: ");
+            double x = double.Parse(Console.ReadLine());
+            double e = 0;
+            int n = 100;
+
+            for(int i =0; i<n; i++){
+                e += (Math.Pow(x, i)) / Factorial(i);
+            }
+            Console.WriteLine("e^x = " + e);
+        }
+        static double Factorial(double i){
+            if (i <= 1) return 1;
+            return i * Factorial(i - 1);
+        }
+    }
+}
