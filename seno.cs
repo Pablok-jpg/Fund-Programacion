afb78d498dfcd5b74fcab940f036b21f9090d894
Pablok-jpg <60201737+Pablok-jpg@users.noreply.github.com>
2020-08-18 23:10:57 -0500

    Add files via upload

---
+++ seno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Seno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallemos el valor del Seno, ingrese x: ");
+            double x = double.Parse(Console.ReadLine());
+            double sen = 0;
+            int n = 100;
+
+            for (int i = 0; i < n; i++){
+                double variable = (2 * i + 1);
+                sen += (Math.Pow((-1), i)) / (Factorial(variable)) * Math.Pow(x, variable);
+            }
+            Console.WriteLine("Sen de x = " + sen);
+        }
+        static double Factorial(double i){
+            if (i <= 1) return 1;
+            return i * Factorial(i - 1);
+        }
+    }
+}
