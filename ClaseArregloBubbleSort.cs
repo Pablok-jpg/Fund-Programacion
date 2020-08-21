aa199f3b8bb3e89a031388894d4679ccc783fc07
Pablok-jpg <60201737+Pablok-jpg@users.noreply.github.com>
2020-08-21 08:31:51 -0500

    Update ClaseArregloBubbleSort.cs

--- ClaseArregloBubbleSort.cs
+++ ClaseArregloBubbleSort.cs
@@ -9,12 +9,11 @@ namespace Trabajo_autónomo_1
         {
             string[] nombres = { "Nezuko", "Tanjiro", "Zenitsu", "Inosuke", "Genya", "Kanao", "Tomioka" };
             double[] estatura = { 153, 165, 164.5, 164, 180, 156, 176 };
-            for (int J = 0; J < nombres.Length; J++)
-            {
-                for (int i = 0; i < nombres.Length - 1; i++)
-                {
-                    if (estatura[i] < estatura[i + 1])
-                    {
+            
+            for (int j = 0; j < nombres.Length; j++){
+                for (int i = 0; i < nombres.Length - 1; i++){
+                    if (estatura[i] < estatura[i + 1]){
+                        
                         string tpmN = nombres[i];
                         nombres[i] = nombres[i + 1];
                         nombres[i + 1] = tpmN;
